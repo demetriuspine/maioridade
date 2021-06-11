935d2cdba9e60838b1be728b4a69db132550db8d
lucas <mr.pine97@gmail.com>
2021-06-11 19:13:14 -0300

    verifica se o usuário é um adolescente, criança ou maior de idade

--- maioridade.py
+++ maioridade.py
@@ -1,5 +1,7 @@
 idade = int(input('Qual a sua idade?'))
 if idade >= 18:
 	print('Você é maior de idade')
+elif idade >= 13 and idade < 18:
+	print('Você é um adolescente')
 else:
-	print('Você não é maior de idade')
+	print('Você é uma criança')
